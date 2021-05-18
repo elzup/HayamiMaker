a9d30ee7b776e33c90287d86a1ee7e6b806935b2
elzup <guild0105@gmail.com>
2021-05-18 23:44:25 +0900

    make type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,31 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
+export type ID = number
+
+export type Item = {
+  id: ID
+  name: string
+  url: string
+}
+
+export type Template = {
+  id: ID
+  name: string
+  items: Item[]
+}
+
+export type Maching = {}
+
+export type Table = {
+  id: ID
+  mirror: boolean
+  templateR: ID
+  templateV: ID
+  cells: Cell[][]
+}
+
+export type Cell = {
+  category: 'emp' | 'val' | 'non' | 'ox'
+  val: number
+  str: string
+}
